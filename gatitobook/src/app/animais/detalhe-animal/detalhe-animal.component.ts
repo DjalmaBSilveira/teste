6906d1ac20cd06b94c4c49de8b57b13563cb1082
Djalma Silveira <djalmasilveira10@gmail.com>
2023-01-27 14:24:19 -0300

    comment input finished

--- gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,15 +1,44 @@
+import { AnimaisService } from './../animais.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animais';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
   templateUrl: './detalhe-animal.component.html',
-  styleUrls: ['./detalhe-animal.component.css']
+  styleUrls: ['./detalhe-animal.component.css'],
 })
 export class DetalheAnimalComponent implements OnInit {
+  animalId!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
+    this.animal$.subscribe((res) => console.log(res));
   }
 
+  curtir() {
+    this.animaisService.curtir(this.animalId).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscaPorId(this.animalId);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalId).subscribe(
+      () => {
+        this.router.navigate(['/animais']);
+      },
+      (error) => console.log(error)
+    );
+  }
 }
